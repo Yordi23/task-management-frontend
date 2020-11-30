637d69329eeb231a59f0f54e5e0a1cc097b85253
Yordi Ogando <yordiogando@gmail.com>
2020-11-29 20:28:51 -0400

    Added build

---
+++ build/precache-manifest.cc3253ff03dcd75636163351dd2ddf4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a2b3709c17f36f0223988f7092d056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6175966ca787a4b269b",
+    "url": "/static/css/main.618379d8.chunk.css"
+  },
+  {
+    "revision": "2dc31e4804f8b7c1d2ee",
+    "url": "/static/js/2.be9c9a3a.chunk.js"
+  },
+  {
+    "revision": "e6175966ca787a4b269b",
+    "url": "/static/js/main.e2a17927.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
